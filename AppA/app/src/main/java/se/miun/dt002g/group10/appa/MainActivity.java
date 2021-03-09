daa2f9b02ed2dec71bacadff84bdc708777bbfa8
Henrik Henriksson <hehe0601@student.miun.se>
2021-03-09 13:44:20 +0100

    Added clear option and about information

--- AppA/app/src/main/java/se/miun/dt002g/group10/appa/MainActivity.java
+++ AppA/app/src/main/java/se/miun/dt002g/group10/appa/MainActivity.java
@@ -1,13 +1,19 @@
 package se.miun.dt002g.group10.appa;
 
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -25,121 +31,169 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-  private static final String SHARED_PREF_APPA = "sharedPrefAppA";
-  private static final String LOG_LIST_KEY_APPA = "logListKeyAppA";
+    private static final String SHARED_PREF_APPA = "sharedPrefAppA";
+    private static final String LOG_LIST_KEY_APPA = "logListKeyAppA";
 
-  private RecyclerView.Adapter mAdapter;
+    private RecyclerView.Adapter mAdapter;
 
-  private RadioGroup radioGroup;
-  private RadioButton radioButton;
-  private ArrayList<LogItem> logItemList;
-  private String intentMsg = null;
-  private SimpleDateFormat sdf;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    private ArrayList<LogItem> logItemList;
+    private String intentMsg = null;
+    private SimpleDateFormat sdf;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    loadLogListItem();
+        loadLogListItem();
 
-    // Set time format
-    sdf = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+        // Set time format
+        sdf = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
 
-    addListenerButton();
+        addListenerButton();
 
-    RecyclerView mRecyclerView = findViewById(R.id.log_recycler_view);
-    mRecyclerView.setHasFixedSize(true);
-    RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
-    mAdapter = new LogItemAdapter(logItemList);
+        RecyclerView mRecyclerView = findViewById(R.id.log_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new LogItemAdapter(logItemList);
 
-    mRecyclerView.setLayoutManager(mLayoutManager);
-    mRecyclerView.setAdapter(mAdapter);
-  }
-
-  @Override
-  protected void onResume() {
-    super.onResume();
-
-    // Get the current system time
-    String currentTime = sdf.format(new Date());
-
-    try {
-      intentMsg = this.getIntent().getExtras().getString(Intent.EXTRA_TEXT);
-      logItemList.add(new LogItem(currentTime, intentMsg));
-    } catch (NullPointerException e) {
-      // No intent received or unable to retrieve content
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
     }
-  }
 
-  @Override
-  protected void onPause() {
-    super.onPause();
-    saveLogListItem();
-  }
-
-  private void saveLogListItem() {
-    SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREF_APPA, MODE_PRIVATE);
-    SharedPreferences.Editor editor = sharedPreferences.edit();
-    Gson gson = new Gson();
-
-    // Store logListItem in json format
-    String json = gson.toJson(logItemList);
-
-    // Add json to shared preferences
-    editor.putString(LOG_LIST_KEY_APPA, json);
-
-    editor.apply();
-  }
-
-  private void loadLogListItem() {
-    SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREF_APPA, MODE_PRIVATE);
-    Gson gson = new Gson();
-    String json = sharedPreferences.getString(LOG_LIST_KEY_APPA, null);
-
-    // Specify that Gson should convert json to ArrayList of LogItems
-    Type type = new TypeToken<ArrayList<LogItem>>() {
-    }.getType();
-    logItemList = gson.fromJson(json, type);
-
-    if (logItemList == null) {
-      logItemList = new ArrayList<>();
-    }
-  }
-
-  private void addListenerButton() {
-    Button intentButton = findViewById(R.id.intentButton);
-    radioGroup = findViewById(R.id.radioGroup);
-
-    intentButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        int selectedId = radioGroup.getCheckedRadioButtonId();
-        radioButton = findViewById(selectedId);
-
-        // Create the text message from the value of the radiobutton
-        Intent implicitIntent = new Intent();
-        implicitIntent.setAction(Intent.ACTION_SEND);
-        implicitIntent.putExtra(Intent.EXTRA_TEXT, radioButton.getText());
-        implicitIntent.setType("text/plain");
+    @Override
+    protected void onResume() {
+        super.onResume();
 
         // Get the current system time
         String currentTime = sdf.format(new Date());
 
-        logItemList.add(new LogItem(currentTime, (String) radioButton.getText()));
-
-        // Update the view
-        mAdapter.notifyDataSetChanged();
-
-        // Try to send implicit intent
         try {
-          startActivity(implicitIntent);
-        } catch (ActivityNotFoundException e) {
-          // Temporary toast
-          Toast.makeText(getApplicationContext(), "Failed to start activity!",
-              Toast.LENGTH_SHORT).show();
+            intentMsg = this.getIntent().getExtras().getString(Intent.EXTRA_TEXT);
+            logItemList.add(new LogItem(currentTime, intentMsg));
+        } catch (NullPointerException e) {
+            // No intent received or unable to retrieve content
         }
-      }
-    });
-  }
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveLogListItem();
+    }
+
+    private void saveLogListItem() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREF_APPA, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+
+        // Store logListItem in json format
+        String json = gson.toJson(logItemList);
+
+        // Add json to shared preferences
+        editor.putString(LOG_LIST_KEY_APPA, json);
+
+        editor.apply();
+    }
+
+    private void loadLogListItem() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREF_APPA, MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString(LOG_LIST_KEY_APPA, null);
+
+        // Specify that Gson should convert json to ArrayList of LogItems
+        Type type = new TypeToken<ArrayList<LogItem>>() {
+        }.getType();
+        logItemList = gson.fromJson(json, type);
+
+        if (logItemList == null) {
+            logItemList = new ArrayList<>();
+        }
+    }
+
+    private void addListenerButton() {
+        Button intentButton = findViewById(R.id.intentButton);
+        radioGroup = findViewById(R.id.radioGroup);
+
+        intentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                radioButton = findViewById(selectedId);
+
+                // Create the text message from the value of the radiobutton
+                Intent implicitIntent = new Intent();
+                implicitIntent.setAction(Intent.ACTION_SEND);
+                implicitIntent.putExtra(Intent.EXTRA_TEXT, radioButton.getText());
+                implicitIntent.setType("text/plain");
+
+                // Get the current system time
+                String currentTime = sdf.format(new Date());
+
+                logItemList.add(new LogItem(currentTime, (String) radioButton.getText()));
+
+                // Update the view
+                mAdapter.notifyDataSetChanged();
+
+                // Try to send implicit intent
+                try {
+                    startActivity(implicitIntent);
+                } catch (ActivityNotFoundException e) {
+                    // Temporary toast
+                    Toast.makeText(getApplicationContext(), "Failed to start activity!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_options, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                showAbout();
+                break;
+            case R.id.action_clear:
+                clearData();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showAbout() {
+
+        AlertDialog.Builder adb = new AlertDialog.Builder(MainActivity.this);
+
+        adb.setTitle(getString(R.string.about_str));
+        adb.setMessage(getString(R.string.about_information));
+        adb.setCancelable(true);
+
+        adb.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                dialog.cancel();
+            }
+
+        });
+        AlertDialog aboutDialog = adb.create();
+        aboutDialog.show();
+    }
+
+    private void clearData() {
+        logItemList.clear();
+        Toast.makeText(getApplicationContext(), R.string.clear_list, Toast.LENGTH_SHORT).show();
+        mAdapter.notifyDataSetChanged();
+    }
+
 }
